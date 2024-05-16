bd2575305e6453f7230f3717a5b888e61ac351bc
Yernar Smagulov <yernar.smagulov02@gmail.com>
2024-05-16 03:13:24 -0700

    fixing a camera issue

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function main() {
     const fov = 60;
     const aspect = 2; // the canvas default
     const near = 0.1;
-    const far = 50;
+    const far = 1000;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.set(0, 10, 20);
 
